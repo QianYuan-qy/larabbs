b96cc305bebb2a511b7c335f6ff4cd8f155acada
QianYuan <875081753@qq.com>
2020-04-17 07:30:04 +0000

    静态浏览器缓存问题

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,17 +2,17 @@
 <html lang="{{ app()->getLocale() }}">
 
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+    <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
-<!-- Styles -->
-<link href="{{ mix('css/app.css') }}" rel="stylesheet">
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
 </head>
 
